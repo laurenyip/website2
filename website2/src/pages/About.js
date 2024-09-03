e1ddffc79f7baf0f80bf2edfacbcd75246731b9e
root <root@dell-ly>
2024-09-03 14:46:39 -0700

    aboutdone

--- website2/src/pages/About.js
+++ website2/src/pages/About.js
@@ -1,28 +1,30 @@
-import React, { useEffect } from 'react';
-import './About.css'; // Assuming you have CSS for styling
+import React, { useEffect } from "react";
+import "./About.css";
+import { Link } from "react-router-dom";
 
 function About() {
   let aboutSlideIndex = 1;
 
   const plusAboutSlides = (n) => {
-    showAboutSlides(aboutSlideIndex += n);
+    showAboutSlides((aboutSlideIndex += n));
   };
 
   const showAboutSlides = (n) => {
-    let i;
     const slides = document.getElementsByClassName("mySlides1");
-    if (n > slides.length) { aboutSlideIndex = 1 }
-    if (n < 1) { aboutSlideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
+    if (n > slides.length) {
+      aboutSlideIndex = 1;
+    }
+    if (n < 1) {
+      aboutSlideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
     slides[aboutSlideIndex - 1].style.display = "block";
   };
 
   useEffect(() => {
-    // Navbar scroll effect
     const header = document.querySelector(".navbar");
-    console.log(header); // Check if this logs the correct element
 
     if (header) {
       const handleScroll = () => {
@@ -34,77 +36,117 @@ function About() {
         }
       };
 
-      window.addEventListener('scroll', handleScroll);
+      window.addEventListener("scroll", handleScroll);
 
       // Initial display of slides
       showAboutSlides(aboutSlideIndex);
 
       return () => {
-        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener("scroll", handleScroll);
       };
     }
   }, []);
 
   return (
-    <section id="about" className="mx-auto mx-5">
-      <div className="row">
+    <div className="About">
+      <ul className="navbar">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home |
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">
+            About |
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/projects">
+            Projects |
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/portfolio">
+            Portfolio
+          </Link>
+        </li>
+      </ul>
+
+      <div className="container">
         <h1 className="text-left">About Me</h1>
-        <div className="container mt-2 pt-3">
-          <div className="row mt-3">
-            <div className="col-lg-3">
+        <div className="row mt-3 about-content">
+          <img
+            src="/images/about/green.png"
+            className="imageAboutPage"
+            alt="Green"
+          />
+
+          <div id="aboutImageSlideshow" className="slideshow-container">
+            <div className="mySlides1">
               <img
-                src="images/heroImage.png"
-                className="imageAboutPage"
-                alt="Hero"
-                style={{ width: '90%', height: 'auto' }}
+                src="images/about/sunrise/1.jpg"
+                alt="Sunrise 1"
+                style={{ width: "54%" }}
               />
             </div>
-            <div className="col-lg-3 text-center">
-              <div id="aboutImageSlideshow" className="slideshow-container">
-                <div className="mySlides1">
-                  <img src="images/sunrise/1.jpg" alt="Sunrise 1" style={{ width: '90%' }} />
-                </div>
-                <div className="mySlides1">
-                  <img src="images/sunrise/2.jpg" alt="Sunrise 2" style={{ width: '90%' }} />
-                </div>
-                <div className="mySlides1">
-                  <img src="images/sunrise/3.jpg" alt="Sunrise 3" style={{ width: '90%' }} />
-                </div>
-                <div className="mySlides1">
-                  <img src="images/sunrise/4.jpg" alt="Sunrise 4" style={{ width: '90%' }} />
-                </div>
-                <div className="mySlides1">
-                  <img src="images/sunrise/5.jpg" alt="Sunrise 5" style={{ width: '90%' }} />
-                </div>
-                <div className="mySlides1">
-                  <img src="images/sunrise/6.jpg" alt="Sunrise 6" style={{ width: '90%' }} />
-                </div>
+            <div className="mySlides1">
+              <img
+                src="images/about/sunrise/2.jpg"
+                alt="Sunrise 2"
+                style={{ width: "54%" }}
+              />
+            </div>
+            <div className="mySlides1">
+              <img
+                src="images/about/sunrise/3.jpg"
+                alt="Sunrise 3"
+                style={{ width: "54%" }}
+              />
+            </div>
+            <div className="mySlides1">
+              <img
+                src="images/about/sunrise/4.jpg"
+                alt="Sunrise 4"
+                style={{ width: "54%" }}
+              />
+            </div>
+            <div className="mySlides1">
+              <img
+                src="images/about/sunrise/5.jpg"
+                alt="Sunrise 5"
+                style={{ width: "54%" }}
+              />
+            </div>
+            <div className="mySlides1">
+              <img
+                src="images/about/sunrise/6.jpg"
+                alt="Sunrise 6"
+                style={{ width: "54%" }}
+              />
+            </div>
 
-                {/* Next button */}
-                <a className="nextA" onClick={() => plusAboutSlides(1)}>&#10095;</a>
-              </div>
-            </div>
-            <div className="col-lg-6">
-              <div className="left-aligned">
-                <p>
-                  Hi, my name is Lauren! My academic interests include visual and
-                  interactive computing, Artificial Intelligence, and UI/UX
-                  design. I'm working on a portfolio of projects to improve my
-                  development and design skills.
-                </p>
-                <p>
-                  In my free time, I like to draw, paint, and read. I also love
-                  playing music and beach volleyball. I watch as many sunrises
-                  and sunsets as possible. I want to explore new cities and the
-                  ocean. (Currently working on my Open Water Diving
-                  certification!)
-                </p>
-              </div>
-            </div>
+            {/* Next button */}
+            <a className="nextA" onClick={() => plusAboutSlides(1)}>
+              &#10095;
+            </a>
+          </div>
+        </div>
+        <div className="col-lg-6">
+          <div className="left-aligned">
+            <p>
+              Hi, my name is Lauren! I'm a 4th year Computer Science student at Simon Fraser University. 
+              My technical interests include visual and interactive computing, Artificial Intelligence, and UI/UX design.
+              I'm working on a portfolio of projects to improve my development and design skills.
+            </p>
+            <p>
+              I also love to read and paint in my spare time, and play the guitar and piano. I love exploring new places on long walks while travelling, 
+              and while I'm here, by hiking and swimming.
+              (Currently working on my Open Water Diving certification!)
+            </p>
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
--- website2/src/pages/Home.js
+++ website2/src/pages/Home.js
@@ -1,56 +1,55 @@
 import "/root/website2/website2/website2/src/App.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-           <ul className="navbar">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              Home |
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/about">
-              About |
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/projects">
-              Projects |
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/portfolio">
-              Portfolio
-            </Link>
-          </li>
-        </ul>
+      <ul className="navbar">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home |
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">
+            About |
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/projects">
+            Projects |
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/portfolio">
+            Portfolio
+          </Link>
+        </li>
+      </ul>
 
-      <div class="container">
-        
-
+      <div class="containermain">
         <p3>YIP/LAUREN宝怡</p3>
-<br></br>
         <br></br>
-
+        <br></br>
         <p3>the website &nbsp; 09FEB 2003</p3>
-<br></br>
-<p2>FM</p2>
+        <br></br>
+        <p2>FM</p2>
         <p3>&nbsp;&nbsp;VANCOUVER/YVR</p3>
         <br></br> <p2>TO</p2>
         <p3>&nbsp;&nbsp;THEWORLD/!!!</p3>
-
-        <br></br><br></br><br></br>
-<p2>&nbsp;&nbsp;Age&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Status</p2> 
-<br></br> <p3b>21A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;YEAR4</p3b> 
-
-<br></br>
-<p3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RESUME</p3>
-<br></br>
-<p2>laurenyip20@gmail.com --- linkedin.com/in/lauren-yip</p2>
-
-    
+        <br></br>
+        <br></br>
+        <br></br>
+        <p2>
+          &nbsp;&nbsp;Age&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Status @ SFU
+        </p2>
+        <br></br> <p3b>21A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;YEAR4</p3b>
+        <br></br>
+        <p3>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RESUME
+        </p3>
+        <br></br>
+        <p2>laurenyip20@gmail.com --- linkedin.com/in/lauren-yip</p2>
       </div>
     </div>
   );
